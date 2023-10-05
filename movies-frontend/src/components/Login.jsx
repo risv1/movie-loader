780c362ef5365c89f5503cc7ac7fded87860cb40
Rishi Viswanathan <128200692+risv1@users.noreply.github.com>
2023-10-05 23:19:17 +0530

    Add files via upload

--- movies-frontend/src/components/Login.jsx
+++ movies-frontend/src/components/Login.jsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
+import Input from "./form/Input";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
 const Login = () => {
-    return(
-        <div className="text-center">
-            <h2>Login</h2>
-            <hr />
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setJwtToken } = useOutletContext();
+  const { setAlertMessage } = useOutletContext();
+  const { setAlertClassName } = useOutletContext();
+
+    const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email/pass", email, password);
+
+    if(email==="admin@example.com"){
+        setJwtToken("abc");
+        setAlertClassName("d-none");
+        setAlertMessage("");
+        navigate("/");
+    }else{
+        setAlertClassName("alert-danger");
+        setAlertMessage("Invalid Credentials")
+    }
+  };
+
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>Login</h2>
+      <hr />
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          autoComplete="password-new"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <hr />
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 export default Login;
