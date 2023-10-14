d2466ff114b5d938616d973026f0fecff7e06102
Rishi Viswanathan <128200692+risv1@users.noreply.github.com>
2023-10-14 15:34:59 +0530

    Postgres Connection

--- movies-backend/cmd/api/routes.go
+++ movies-backend/cmd/api/routes.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
-	// create router mux (multiplexer)
+	// create a router mux
 	mux := chi.NewRouter()
 
-	mux.Use(middleware.Recoverer) // when application errors, logs with location of error, and brings appl back 
+	mux.Use(middleware.Recoverer)
 	mux.Use(app.enableCORS)
 
 	mux.Get("/", app.Home)
+
 	mux.Get("/movies", app.AllMovies)
 
 	return mux
